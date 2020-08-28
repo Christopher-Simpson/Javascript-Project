ff7a0415ad13f952d6766474c7a5809264d74649
Chris Simpson <chris_simpson@live.com.au>
2020-08-28 20:52:06 +0000

    cards generated to board

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-//Card objects array
 const cardObj = [
-    {name: 'bramble', img: 'images/bramble.png'},
-    {name: 'bramble', img: 'images/bramble.png'},
-    {name: 'highball', img: 'images/highball.png'},
-    {name: 'highball', img: 'images/highball.png'},
-    {name: 'julep', img: 'images/julep.png'},
-    {name: 'julep', img: 'images/julep.png'},
-    {name: 'martini', img: 'images/martini.png'},
-    {name: 'martini', img: 'images/martini.png'},
-    {name: 'mint', img: 'images/mint.png'},
-    {name: 'mint', img: 'images/mint.png'},
-    {name: 'wine', img: 'images/wine.png'},
-    {name: 'wine', img: 'images/wine.png'}
+    {name: 'bramble', image: 'images/bramble.png'},
+    {name: 'bramble', image: 'images/bramble.png'},
+    {name: 'highball', image: 'images/highball.png'},
+    {name: 'highball', image: 'images/highball.png'},
+    {name: 'julep', image: 'images/julep.png'},
+    {name: 'julep', image: 'images/julep.png'},
+    {name: 'martini', image: 'images/martini.png'},
+    {name: 'martini', image: 'images/martini.png'},
+    {name: 'mint', image: 'images/mint.png'},
+    {name: 'mint', image: 'images/mint.png'},
+    {name: 'wine', image: 'images/wine.png'},
+    {name: 'wine', image: 'images/wine.png'}
 ]
 
-//loop through array creating an img element from each object and render to HTML
+
+
 function dealCards() {
-    for (let i = 0; 0 < cardObj.length; i++) {
+    for (let i = 0; i < cardObj.length; i++) {
         var container = document.querySelector('.container')
         var cards = document.createElement('img')
+        cards.setAttribute('src', 'images/marble.jpg')
         container.appendChild(cards)
     }
 }
